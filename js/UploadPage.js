473d852e6a93e44c76c79db33dfd410292aa3a37
eunbi <rladmsql710@naver.com>
2022-03-14 18:43:37 +0900

    메인 수정하고 판매리스트페이지까지 판매리스트까지

--- js/UploadPage.js
+++ js/UploadPage.js
@@ -1,29 +1,51 @@
-$(document).ready(function () {
-    if (window.File && window.FileList && window.FileReader) {
-        $("#files").on("change", function (e) {
-            var files = e.target.files,
-                filesLength = files.length;
-            for (var i = 0; i < filesLength; i++) {
-                var f = files[i]
-                var fileReader = new FileReader();
-                fileReader.onload = (function (e) {
-                    var file = e.target;
-                    $("<span class=\"pip\">" +
-                        "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
-                        "<br/><span class=\"remove\">x</span>" +
-                        "</span>").insertAfter("#files");
-                    $(".remove").click(function () {
-                        $(this).parent(".pip").remove();
-                        $(File).remove();
-                    });
-                });
-                fileReader.readAsDataURL(f);
+$(".imgAdd").click(function () {
+    $(this).closest(".row").find('.imgAdd').before('<div class="col-sm-2 imgUp"><div class="imagePreview"></div><label class="btn btn-primary">Upload<input type="file" class="uploadFile img" value="Upload Photo" style="width:0px;height:0px;overflow:hidden;"></label><i class="fa fa-times del"></i></div>');
+});
+$(document).on("click", "i.del", function () {
+    $(this).parent().remove();
+});
+$(function () {
+    $(document).on("change", ".uploadFile", function () {
+        var uploadFile = $(this);
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+
+        if (/^image/.test(files[0].type)) { // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+
+            reader.onloadend = function () { // set image data as background of div
+                //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+                uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url(" + this.result + ")");
             }
-            console.log(files);
-        });
-    } else {
-        alert("Your browser doesn't support to File API")
-    }
+        }
+
+    });
 });
 
 
+// 메인이미지 js 코드
+$(".imgAdd").click(function () {
+    $(this).closest(".row").find('.imgAdd').before('<div class="col-sm-2 imgUp"><div class="imagePreview"></div><label class="btn btn-primary">Upload<input type="file" class="uploadFile img" value="Upload Photo" style="width:0px;height:0px;overflow:hidden;"></label><i class="fa fa-times del"></i></div>');
+});
+$(document).on("click", "i.del", function () {
+    $(this).parent().remove();
+});
+$(function () {
+    $(document).on("change", ".uploadFile", function () {
+        var uploadFile = $(this);
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+
+        if (/^image/.test(files[0].type)) { // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+
+            reader.onloadend = function () { // set image data as background of div
+                //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+                uploadFile.closest(".imgUpMain").find('.imagePreviewMain').css("background-image", "url(" + this.result + ")");
+            }
+        }
+
+    });
+});
