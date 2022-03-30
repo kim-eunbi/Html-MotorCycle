2506aaeaddce8f139f5c43e2f7403bc48ed41c18
eunbi <rladmsql710@naver.com>
2022-03-30 18:15:19 +0900

    마이페이지 반응형해 반응형

--- js/moveScroll.js
+++ js/moveScroll.js
@@ -4,6 +4,6 @@ $(document).ready(function () {
     const currentPosition = parseInt($(".quickmenu").css("top"));
     $(window).scroll(function () {
         var position = $(window).scrollTop();
-        $(".quickmenu").stop().animate({ "top": position + currentPosition + "px" }, 600);
+        $(".quickmenu").stop().animate({ "top": position + currentPosition + "px" }, 1000);
     });
 });
